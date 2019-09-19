9a04be3a2ae3972d7d1243040802a8a00c569c8a
Colleen O'Rourke <colleen@sentry.io>
2019-09-19 12:49:31 -0700

    testing

--- testing.py
+++ testing.py
@@ -1,21 +1,6 @@
 import sentry_sdk
-from sentry_sdk import capture_message
-from sentry_sdk import capture_exception
-from sentry_sdk import configure_scope
-import time
-import os
-
 sentry_sdk.init(
-	dsn='https://5d0ad1da55464d9e822c593677e76435@sentry.io/1232413',
-    # dsn=os.environ.get('SENTRY_DSN'),
-    release=os.environ.get('VERSION')
-)
-
-with configure_scope() as scope:
-    scope.user = {"email": "testing@example.com"}
-
-try:
-    hi_there()
-except Exception as e:
-    capture_exception(e)
-raise
+	dsn='https://5a47dfa361a44e35aa933b700c9c9e46@sentry.io/1499922',
+	release='abc555'
+	)
+gonna_break = 1 / 0
